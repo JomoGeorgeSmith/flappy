8c1f19348b4c16e578f0773fef9bde299c8eb212
Jomo Smith <jomogeorgesmith@gmail.com>
2024-02-11 03:47:59 -0500

    Saving

--- game.py
+++ game.py
@@ -5,8 +5,9 @@ import neat
 import time
 import os 
 import random 
+pygame.font.init()
 
-WIN_WIDTH = 600
+WIN_WIDTH = 550
 WIN_HEIGHT = 800 
 
 BIRD_IMGS = [
@@ -18,6 +19,7 @@ BIRD_IMGS = [
 PIPE_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","pipe.png")))]
 BASE_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","base.png")))]
 BG_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bg.png")))]
+STAT_FONT = pygame.font.SysFont("comicsans", 50)
 
 class Bird:
     IMGS = BIRD_IMGS
@@ -84,22 +86,125 @@ class Bird:
     
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
+class Pipe:
+    GAP = 200;
+    VEL =  5
 
-def draw_window(win, bird):
+    def __init__(self,x):
+        self.x = x 
+        self.height = 0 
+        self.top = 0 
+        self.bottom = 0 
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMGS[0], False, True)
+        self.PIPE_BOTTOM = PIPE_IMGS[0]
+        self.passed = False 
+        self.set_height()
+
+    def set_height(self):
+        self.height = random.randrange(50,450)
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+    
+    def move(self):
+        self.x -= self.VEL
+    
+    def draw(self, win):
+        win.blit(self.PIPE_TOP, (self.x, self.top))
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+    
+    def collide(self, bird):
+        bird_mask = bird.get_mask()
+        top_mask =  pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask =  pygame.mask.from_surface(self.PIPE_BOTTOM)
+
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        t_point = bird_mask.overlap(top_mask, top_offset)
+
+        if t_point or b_point:
+            return True
+        
+        return False 
+class Base:
+    VEL = 5
+    WIDTH = BASE_IMGS[0].get_width()
+    IMG = BASE_IMGS
+
+    def __init__(self, y ):
+        self.y = y 
+        self.x1 = 0 
+        self.x2 = self.WIDTH
+    
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH < 0 :
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0 :
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        win.blit(self.IMG[0], (self.x1, self.y))
+        win.blit(self.IMG[0], (self.x2,self.y))
+
+def draw_window(win, bird, pipes, base, score):
     win.blit(BG_IMGS[0], (0,0))
+
+    for pipe in pipes:
+        pipe.draw(win)
+
+    text = STAT_FONT.render("Score: " + str(score), 1, (255,255,255))
+    win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
+    base.draw(win)
+
     bird.draw(win)
     pygame.display.update()
 
 def main():
-    bird = Bird(200,200)
+    bird = Bird(230,350)
+    base = Base(730)
+    pipes = [Pipe(700)]
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    score = 0 
 
     run = True
     while run: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False 
-        draw_window(win, bird);
+
+        base.move()
+
+        rem = []
+        add_pipe = False
+        for pipe in pipes:
+            if pipe.collide(bird):
+                pass
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0 :
+                rem.append(pipe)
+
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True 
+
+            pipe.move()
+
+        if add_pipe:
+            score += 1
+            pipes.append(Pipe(700))
+        
+        for r in rem:
+            pipes.remove(r)
+
+        if bird.y + bird.img.get_height() >= 730:
+            pass
+
+
+        draw_window(win, bird, pipes, base, score);
     pygame.quit()
     quit()
 main()
